f659f026e43e8ab1e7e3465bb3b2621f0a1fe6ad
eric <elavagni@gmail.com>
2020-09-13 18:06:24 -0500

    first implementation for signup route, leverage errorHandler and custom errors

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,8 @@ import { currentUserRouter } from './routes/current-user'
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 
@@ -13,6 +15,12 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+
+app.all('*', () => {
+    throw new NotFoundError();
+});
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
     console.log('Listening on port 3000!!');
