d204a6f5903f875e5be566f110e2ea8b35eb4e23
eric <elavagni@gmail.com>
2020-09-19 21:37:40 -0500

    Refactored test to use helper method. Add test for non-authenticated user

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -2,15 +2,7 @@ import request from 'supertest';
 import { app } from '../../app';
 
 it('responds with details about the current user', async () => {
-    const signupResponse = await request(app)
-      .post('/api/users/signup')
-      .send({
-        email: 'test@test.com',
-        password: 'password'
-      })
-    .expect(201);
-
-    const cookie = signupResponse.get('Set-Cookie');
+    const cookie = await global.getAuthCookie();
 
     const response=  await request(app)
         .get('/api/users/currentuser')
@@ -20,3 +12,12 @@ it('responds with details about the current user', async () => {
 
     expect(response.body.currentUser.email).toEqual('test@test.com');
   });
+
+it('responds with null if not authenticated', async () => {
+  const respose = await request(app)
+    .get('/api/users/currentuser')
+    .send()
+    .expect(200);
+    
+    expect(respose.body.currentUser).toEqual(null);
+});
