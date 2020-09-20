a3299c0582a3733d340f2e95d07c525842d08d9e
eric <elavagni@gmail.com>
2020-09-19 21:36:40 -0500

    Create helper function to get the Authentication  cookie

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,5 +1,15 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
+import request from 'supertest';
+import { app } from "../app";
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      getAuthCookie(): Promise<string[]>;
+    }
+  }  
+}
 
 let mongo: any;
 beforeAll(async () => {
@@ -26,3 +36,20 @@ afterAll(async () => {
   await mongo.stop();
   await mongoose.connection.close();
 });
+
+//Create globally scoped function for signin, it is global for ease of use.
+global.getAuthCookie = async () => {
+  const email = 'test@test.com';
+  const password = 'password';
+
+  const response = await request(app)
+      .post('/api/users/signup')
+      .send({
+        email, password
+      })
+      .expect(201)
+
+  const cookie = response.get('Set-Cookie');
+
+  return cookie;
+}
