220d68992e7016122c03e504684fbd0d82f1dbb2
eric <elavagni@gmail.com>
2020-09-20 22:49:45 -0500

    Add signup form, and call backend

---
+++ client/pages/auth/signup.js
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+import axios from 'axios';
+
+export default () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [errors, setErrors] = useState([]);
+
+    const onSubmit =  async event => {
+        event.preventDefault();   
+        try {            
+            const response = await axios.post('/api/users/signup', {
+                email, password
+            });
+                console.log(response.data);   
+        } catch (error) {
+            setErrors(error.response.data.errors);
+        }
+    } 
+
+    return (
+        <form onSubmit={onSubmit}>
+            <h1>Sign Up</h1>
+            <div className="form-group">
+                <label>Email Address</label>
+                <input 
+                    value={email} 
+                    onChange={event => setEmail(event.target.value) } 
+                    className="form-control"
+                />
+            </div>
+            <div className="form-group">
+                <label>Password</label>
+                <input 
+                    type="password" 
+                    value={password} 
+                    onChange={event => setPassword(event.target.value) } 
+                    className="form-control"
+                />
+            </div>
+            {errors.length> 0 &&
+                <div className="alert alert-danger">
+                    <h4>Ooooops...</h4>
+                    <ul className="my-0">
+                        {errors.map((error,index) => <li key={index}>{error.message}</li>)}
+                    </ul>
+                </div>
+            }
+            <button className="btn btn-primary">Sign up</button>
+        </form>
+    );
+};
