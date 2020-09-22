3a92982b15e7e0a76b473e4452f1809f9fc3eaa7
eric <elavagni@gmail.com>
2020-09-21 23:42:48 -0500

    Create custom hook to handle common backened calls from different  components

---
+++ client/hooks/use-request.js
@@ -0,0 +1,35 @@
+import axios from 'axios'
+import { useState } from 'react'
+
+export default ({ url, method, body, onSuccess }) => {
+    const [errors, setErrors] = useState(null);
+ 
+    const doRequest = async () => {
+        try {
+            setErrors(null);
+            const response = await axios[method](url, body);
+
+            if (onSuccess) {
+                onSuccess(response.data);
+            }
+
+            return response.data;
+        } catch (error) {
+            setErrors(
+            <div className="alert alert-danger">
+                <h4>Ooooops...</h4>
+                <ul className="my-0">
+                    {error.response.data.errors.map((error,index) => (
+                        <li key={index}>{error.message}</li>
+                    ))}
+                </ul>
+            </div>
+            );
+        }
+
+    };  
+    
+    return { doRequest, errors };
+
+};
+
--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,21 +1,23 @@
 import { useState } from 'react';
-import axios from 'axios';
+import Router from 'next/router';
+import useRequest from "../../hooks/use-request";
 
 export default () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [errors, setErrors] = useState([]);
-
+    const { doRequest, errors} = useRequest({
+        url: '/api/users/signup',
+        method: 'post',
+        body: {
+            email, password
+        },
+        onSuccess: () =>  Router.push('/')
+    });
     const onSubmit =  async event => {
         event.preventDefault();   
-        try {            
-            const response = await axios.post('/api/users/signup', {
-                email, password
-            });
-                console.log(response.data);   
-        } catch (error) {
-            setErrors(error.response.data.errors);
-        }
+        
+        doRequest();
+
     } 
 
     return (
@@ -38,14 +40,7 @@ export default () => {
                     className="form-control"
                 />
             </div>
-            {errors.length> 0 &&
-                <div className="alert alert-danger">
-                    <h4>Ooooops...</h4>
-                    <ul className="my-0">
-                        {errors.map((error,index) => <li key={index}>{error.message}</li>)}
-                    </ul>
-                </div>
-            }
+            {errors}
             <button className="btn btn-primary">Sign up</button>
         </form>
     );
