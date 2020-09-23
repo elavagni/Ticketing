c8da72c4fb00832f67b7e81158f9c161606c7b81
eric <elavagni@gmail.com>
2020-09-22 21:51:36 -0500

    Get current user during SSR

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,32 @@
-export default () => {
+import axios  from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+    //Log current user coming fro, getInitialProps 
+    console.log(currentUser)
     return <h1>Landing Page</h1>;
+}
+
+LandingPage.getInitialProps = async ({ req }) => {
+    
+    if(typeof window === 'undefined') {
+        //we are on the server!
+        //requests should be made to http://SERVICENAME.NAMESPACE.svc.cluster.local{api-route}
+        const { data }  = await axios.get(            
+            'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', 
+            {
+                headers: req.headers                
+            }            
+        );
+        return data;
+        
+    } else {
+        //we are in the browser!
+        //request can be made with a base url of ''
+        const { data }  = await axios.get('/api/users/currentuser');
+        return data;
+    }
+    console.log('I WAS EXECUTED');
+    return {};
 };
+
+export default LandingPage;
