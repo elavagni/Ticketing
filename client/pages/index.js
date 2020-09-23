8e0954a301f1bb42a275d8848435444a49764886
eric <elavagni@gmail.com>
2020-09-22 22:14:50 -0500

    Create a reusable http client

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,4 +1,4 @@
-import axios  from 'axios';
+import buildHttpClient  from '../api/build-http-client';
 
 const LandingPage = ({ currentUser }) => {
     //Log current user coming fro, getInitialProps 
@@ -6,27 +6,11 @@ const LandingPage = ({ currentUser }) => {
     return <h1>Landing Page</h1>;
 }
 
-LandingPage.getInitialProps = async ({ req }) => {
-    
-    if(typeof window === 'undefined') {
-        //we are on the server!
-        //requests should be made to http://SERVICENAME.NAMESPACE.svc.cluster.local{api-route}
-        const { data }  = await axios.get(            
-            'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', 
-            {
-                headers: req.headers                
-            }            
-        );
-        return data;
-        
-    } else {
-        //we are in the browser!
-        //request can be made with a base url of ''
-        const { data }  = await axios.get('/api/users/currentuser');
-        return data;
-    }
-    console.log('I WAS EXECUTED');
-    return {};
+LandingPage.getInitialProps = async context => {
+    const httpClient = buildHttpClient(context);
+    const { data } = await httpClient.get('/api/users/currentuser');
+
+    return data;
 };
 
 export default LandingPage;
