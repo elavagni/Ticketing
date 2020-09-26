509fe3fc557ff567ebeb4aa2b5a795248197df6e
eric <elavagni@gmail.com>
2020-09-26 18:02:47 -0500

    Add header component to main app component

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,33 @@
-import "bootstrap/dist/css/bootstrap.css";
+import 'bootstrap/dist/css/bootstrap.css';
+import builHttpClient from '../api/build-http-client';
+import Header from '../components/header';
 
-export default ({ Component, pageProps }) => {
-    return <Component {...pageProps} />
+const AppComponent =  ({ Component, pageProps, currentUser }) => {
+    return (
+        <div>
+            <Header currentUser={currentUser} />
+            <Component {...pageProps} /> 
+        </div>     
+    );
 };
+
+//The arguments provided to the getInitialProps function are going to be different if we are in 
+//a page versus when we are in a component.
+AppComponent.getInitialProps = async (appContext) => {
+    const httpClient = builHttpClient(appContext.ctx);
+    const { data } = await httpClient.get('/api/users/currentuser');
+    let pageProps = {};
+    
+    if (appContext.Component.getInitialProps) {
+        pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+        console.log(pageProps);
+    }
+    return {
+        pageProps,    
+        currentUser: data.currentUser
+        //we could also do ...data here
+    }
+    
+}
+
+export default AppComponent;
