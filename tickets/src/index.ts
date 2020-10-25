036dccabe07b322f107b3bad44e737b1527b4491
eric <elavagni@gmail.com>
2020-10-25 12:49:48 -0500

    Add NATS settings to config file as env variables

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,17 +1,47 @@
 import mongoose from 'mongoose';
 import { app } from './app';
+import { natsWrapper } from './nats-wrapper';
 
 const start = async () => {
 
-    if(!process.env.JWT_KEY) {
+    if (!process.env.JWT_KEY) {
         throw new Error('JWT_KEY must be defined');
     }
 
-    if(!process.env.MONGO_URI) {
+    if (!process.env.MONGO_URI) {
         throw new Error('MONGO_URI must be defined');
     }    
+
+    if (!process.env.NATS_CLIENT_ID) {
+        throw new Error('NATS_CLIENT_ID must be defined');
+    }   
+
+    if (!process.env.NATS_URL) {
+        throw new Error('NATS_URL must be defined');
+    }   
+
+    if (!process.env.NATS_CLUSTER_ID) {
+        throw new Error('NATS_CLUSTER_ID must be defined');
+    }   
+
     
-    try{
+    try {
+        await natsWrapper.connect(
+            process.env.NATS_CLUSTER_ID,
+            process.env.NATS_CLIENT_ID,
+            process.env.NATS_URL,
+        );
+
+        natsWrapper.client.on('close', () => {
+            console.log('NATS connection closed!');
+            process.exit();
+        });    
+        
+        //Manual restart or shutdown 
+        process.on('SIGINT', () => natsWrapper.client.close());
+        //This one won't work in windows
+        process.on('SIGTERM', () => natsWrapper.client.close());
+        
         //instead of using localhost, use the name of the cluster ip service that holds the mongodb database 
         await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
