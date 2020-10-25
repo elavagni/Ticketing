5a9a91657abf702570564ff0b3b52c4491bfa2e3
eric <elavagni@gmail.com>
2020-10-25 12:39:24 -0500

    Publish ticket updated event

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -7,6 +7,8 @@ import {
     NotAuthorizedError
 } from '@eltickets/common';
 import { Ticket }  from '../models/ticket';
+import { TicketUpdatedPublisher } from '../events/publishers/ticket-updated-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -38,6 +40,13 @@ async (req: Request, res: Response) => {
     });
 
     await ticket.save();
+
+    new TicketUpdatedPublisher(natsWrapper.client).publish({
+        id: ticket.id,
+        title: ticket.title,
+        price: ticket.price, 
+        userId: ticket.userId
+    });
 
     res.send(ticket);
 });
