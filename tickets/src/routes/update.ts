9fb73d45e48b1f2c2c01cf07e43c6dbd55ba3135
eric <elavagni@gmail.com>
2020-11-06 21:39:20 -0600

    Add ticket version when publishing ticket events (created, updated)

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -43,6 +43,7 @@ async (req: Request, res: Response) => {
 
     new TicketUpdatedPublisher(natsWrapper.client).publish({
         id: ticket.id,
+        version: ticket.version,
         title: ticket.title,
         price: ticket.price, 
         userId: ticket.userId
