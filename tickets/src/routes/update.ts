fc68aa591f8470eb92a59faf757ef2171fdc55cf
eric <elavagni@gmail.com>
2020-11-14 13:57:46 -0600

    Throw error if the ticket is already reserved (has an orderId property)

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -4,7 +4,8 @@ import {
     validateRequest,
     NotFoundError, 
     requireAuth,
-    NotAuthorizedError
+    NotAuthorizedError,
+    BadRequestError
 } from '@eltickets/common';
 import { Ticket }  from '../models/ticket';
 import { TicketUpdatedPublisher } from '../events/publishers/ticket-updated-publisher';
@@ -28,6 +29,10 @@ async (req: Request, res: Response) => {
 
     if (!ticket) {
         throw new NotFoundError();
+    }
+
+    if (ticket.orderId) {
+        throw new BadRequestError('Cannot edit a reserved ticket');
     }
 
     if (ticket.userId !== req.currentUser!.id) {
