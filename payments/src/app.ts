8310a34ebb5c1a9b28b64b6ffe621b2ed938c49a
eric <elavagni@gmail.com>
2020-11-17 21:56:16 -0600

    Tell express to use the new route

--- payments/src/app.ts
+++ payments/src/app.ts
@@ -3,6 +3,7 @@ import 'express-async-errors'
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session'
 import { errorHandler, NotFoundError, currentUser } from '@eltickets/common';
+import { createChargeRouter } from './routes/new';
 
 const app = express();
 app.set('trust proxy', true);
@@ -17,6 +18,7 @@ app.use(
 );
 
 app.use(currentUser);
+app.use(createChargeRouter);
 
 app.all('*', async (req, res) => {
     throw new NotFoundError();
