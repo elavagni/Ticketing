910ea91fdaeec5b40b899417dca20eb6d9bbc10e
eric <elavagni@gmail.com>
2020-11-18 22:35:19 -0600

    Store order and payment information together

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -10,6 +10,7 @@ import {
 } from '@eltickets/common';
 import { Order } from '../models/order';
 import { stripe } from '../stripe';
+import { Payment } from '../models/payment';
 
 const router = express.Router();
 
@@ -41,12 +42,19 @@ router.post('/api/payments',
             throw new BadRequestError('Cannot pay for an cancelled order');
         }
 
-        await stripe.charges.create({
-          currency: 'usd',
-          amount: order.price * 100,
-          source: token
+        const charge = await stripe.charges.create({
+            currency: 'usd',
+            amount: order.price * 100,
+            source: token
         });
 
+        const payment = Payment.build({
+          orderId: orderId,
+          stripeId: charge.id
+        });
+
+        await payment.save();
+        
         res.status(201).send({ success: true });
     }
 );
