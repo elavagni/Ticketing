26a6e10d0b9db8a7135afb39a310052ef70110a3
eric <elavagni@gmail.com>
2020-11-18 22:45:50 -0600

    Publish payment created event

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -11,6 +11,8 @@ import {
 import { Order } from '../models/order';
 import { stripe } from '../stripe';
 import { Payment } from '../models/payment';
+import { PaymentCreatedPublisher } from '../events/publishers/payment-created-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -54,8 +56,13 @@ router.post('/api/payments',
         });
 
         await payment.save();
+        new PaymentCreatedPublisher(natsWrapper.client).publish({
+          id: payment.id,
+          orderId: payment.id, 
+          stripeId: charge.id
+        });
         
-        res.status(201).send({ success: true });
+        res.status(201).send({ id: payment.id });
     }
 );
 
