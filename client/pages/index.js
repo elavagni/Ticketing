c826ecbd7a20046ccb021f27f18a9d7522ff9c0b
eric <elavagni@gmail.com>
2020-11-22 16:11:45 -0600

    Display all tickets, and provide links to specific tickets

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,13 +1,50 @@
-import buildHttpClient  from '../api/build-http-client';
+import Link from 'next/link';
 
-const LandingPage = ({ currentUser }) => {
-    //currentUser is comming from getInitialProps 
-    return currentUser ? <h1>You are signed in</h1> : <h1>You are not signed in</h1> 
+const LandingPage = ({ currentUser, tickets }) => {
+
+  const ticketList = tickets.map(ticket => {
+    return (
+      <tr key={ticket.id}>
+        <td>{ticket.title}</td>
+        <td>{ticket.price}</td>
+        <td>
+          <Link href="tickets/[ticketId]" as={`/tickets/${ticket.id}`}>
+            <a>View</a>
+          </Link>
+        </td>
+      </tr>
+      );
+  });  
+
+  return (
+    <div>
+      <h1>Tickets</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+            <th>Link</th>
+            </tr>
+        </thead>
+        <tbody>
+          { ticketList }
+        </tbody>
+      </table>
+    </div>
+  )
     
 }
 
-LandingPage.getInitialProps = async context => {
-   return {};
+LandingPage.getInitialProps = async (context, httpClient, currentUser) => {
+  //return {};
+
+  //STORY GET TICKETS
+
+  const { data } = await httpClient.get('/api/tickets');
+
+  return { tickets: data };
+
 };
 
 export default LandingPage;
